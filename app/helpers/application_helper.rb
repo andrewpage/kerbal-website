877bd35f8c53e3828f673126f97ebdfdda1ab486
Andrew Page <andrew@andrewpage.me>
2014-04-03 00:20:33 -0700

    Navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def site_title(given)
-  	(given.nil? || given.blank?) ? Settings.general.site_title : (given + ' - ' + Settings.general.site_title_short)
-  end
+	def cp(path)
+		'active' if current_page?(path)
+	end	
+	
+	def site_title(given)
+		(given.nil? || given.blank?) ? Settings.general.site_title : (given + ' - ' + Settings.general.site_title_short)
+	end
 end
