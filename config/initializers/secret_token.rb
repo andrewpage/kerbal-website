26817e2d5abc0351f598267ef100c9d36bf572e2
Andrew Page <andrew@andrewpage.me>
2014-04-02 20:26:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kerbalsite::Application.config.secret_key_base = '480db79b4790da1b6c56ae4d71bc6d051c190d833fb445b5ec4564c41c76e4af77cd136f366f667f54f53eb6f83c7c6c6ac5a6d48b2ed5a949524d63a0f6cce4'
