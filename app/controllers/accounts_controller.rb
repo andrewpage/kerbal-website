a6c2a09fe00d3c7eb4a0af82b0ef407250efaff3
Andrew Page <andrew@andrewpage.me>
2014-04-03 23:47:33 -0700

    Show account;

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,8 @@
 class AccountsController < ApplicationController
+	def show
+		@user = Account.find(params[:id])
+	end
+
 	private
 	def account_params
 		params.require(:account).permit(:username, :first_name, :last_name, :country)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Kerbalsite::Application.routes.draw do
   devise_for :accounts, path: 'auth'
 
+  resources :accounts
+
   match 'contact', to: 'pages#contact', via: 'get'
 
   root to: 'pages#home'
