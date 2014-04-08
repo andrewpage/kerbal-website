2e3b0fe7656691d0b02cee3eb6872fe36f0bd660
Andrew Page <andrew@andrewpage.me>
2014-04-08 01:03:24 -0700

    Added Font-Awesome to precompile

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module Kerbalsite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
+    config.assets.precompile += ["fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff"]
   end
 end
